eba04acb709513814e8bb4da0e4bc95cdced1866
meh <meh@schizofreni.co>
2013-09-09 13:52:36 +0200

    listener: add some specs and docs

--- lib/reagent/listener.ex
+++ lib/reagent/listener.ex
@@ -7,12 +7,24 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 defrecord Reagent.Listener, socket: nil, id: nil, pool: nil, module: nil, address: nil, port: nil, secure: nil, acceptors: 1, options: [], details: nil do
+  @doc """
+  Check if the connection is secure or not.
+  """
+  @spec secure?(t) :: boolean
   def secure?(__MODULE__[secure: nil]), do: false
   def secure?(__MODULE__[]),            do: true
 
+  @doc """
+  Get the certificate of the listener.
+  """
+  @spec cert(t) :: String.t
   def cert(__MODULE__[secure: nil]), do: nil
   def cert(__MODULE__[secure: sec]), do: sec[:cert]
 
+  @doc """
+  Convert the listener to `Socket.options` compatible options.
+  """
+  @spec to_options(t) :: Keyword.t
   def to_options(__MODULE__[address: address, options: options, secure: nil]) do
     Keyword.merge(options, local: [address: address], automatic: false)
   end
