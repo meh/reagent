e56f2f839df6cfb89beb1999920ccd2aac68fcf9
Christoph Grabo <chris@dinarrr.com>
2014-03-23 14:33:36 +0100

    Make application compatible for OTP releases
    Details: https://github.com/meh/reagent/pull/2

--- mix.exs
+++ mix.exs
@@ -2,23 +2,19 @@ defmodule Reagent.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :reagent,
-      version: "0.0.1",
-      elixir: "~> 0.12.3",
-      deps: deps ]
+    [ app:     :reagent,
+      version: "0.0.3",
+      elixir:  "~> 0.12.5",
+      deps:    deps ]
   end
 
-  # Configuration for the OTP application
   def application do
-    [ applications: [:socket, :derp] ]
+    [ applications: [:datastructures, :exts, :socket] ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :socket, github: "meh/elixir-socket" },
+    [ { :socket,         github: "meh/elixir-socket" },
       { :datastructures, github: "meh/elixir-datastructures" },
-      { :exts, github: "meh/exts" },
-      { :derp, github: "meh/derp" } ]
+      { :exts,           github: "meh/exts" } ]
   end
 end
