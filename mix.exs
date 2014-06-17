2c6bc07a012dfced796cb0ebeefe8006c696a88b
meh <meh@schizofreni.co>
2014-06-17 20:25:48 +0200

    mix: bump version and Elixir requirement

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Reagent.Mixfile do
 
   def project do
     [ app:     :reagent,
-      version: "0.1.1",
-      elixir:  "~> 0.13.2",
+      version: "0.1.2",
+      elixir:  "~> 0.14.0",
       deps:    deps,
       package: package,
       description: "You need more reagents to conjure this server" ]
@@ -16,13 +16,13 @@ defmodule Reagent.Mixfile do
   end
 
   defp deps do
-    [ { :socket, "~> 0.2.2" },
-      { :exts,   "~> 0.1.1" } ]
+    [ { :socket, "~> 0.2.4" },
+      { :exts,   "~> 0.1.2" } ]
   end
 
   defp package do
     [ contributors: ["meh"],
-      license: "WTFPL",
+      licenses: ["WTFPL"],
       links: [ { "GitHub", "https://github.com/meh/reagent" } ] ]
   end
 end
