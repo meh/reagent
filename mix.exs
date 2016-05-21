b1bfaeb9be17f8a19aacbdc644f29dc61cf255c0
meh <meh@schizofreni.co>
2016-05-21 09:34:58 +0200

    mix: remove Elixir requirement and bump version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,7 @@ defmodule Reagent.Mixfile do
 
   def project do
     [ app:     :reagent,
-      version: "0.1.6",
-      elixir:  "~> 1.0.0-rc1 or ~> 1.1.0-rc.0 or ~> 1.2.0-rc.0",
+      version: "0.1.7",
       deps:    deps,
       package: package,
       description: "You need more reagents to conjure this server" ]
@@ -16,8 +15,8 @@ defmodule Reagent.Mixfile do
   end
 
   defp deps do
-    [ { :socket, "~> 0.2.6" },
-      { :exts,   "~> 0.2.0" } ]
+    [ { :socket, "~> 0.3" },
+      { :exts,   "~> 0.2" } ]
   end
 
   defp package do
