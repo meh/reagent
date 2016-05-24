d4160c9dda6d1fd8fde829e7dfc808617ebdb883
meh <meh@schizofreni.co>
2016-05-24 19:33:50 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Reagent.Mixfile do
 
   def project do
     [ app:     :reagent,
-      version: "0.1.9",
+      version: "0.1.10",
       deps:    deps,
       package: package,
       description: "You need more reagents to conjure this server" ]
